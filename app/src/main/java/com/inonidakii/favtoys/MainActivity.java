3dcc0d4dd3fbb1cb572119f8d1fd330c3c9191da
Aaron Edaku <eaaron25@gmail.com>
2022-04-16 10:23:05 +0300

    Done did the TO-DOs

--- app/src/main/java/com/inonidakii/favtoys/MainActivity.java
+++ app/src/main/java/com/inonidakii/favtoys/MainActivity.java
@@ -3,20 +3,27 @@ package com.inonidakii.favtoys;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-//    TODO (1) Declare a TExtView variable  called eToysListTextView
+//    TO-DO (1) Declare a TextView variable  called eToysListTextView
+    TextView eToysTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        TODO (3) Use findViewById to get a reference to the TextView from the layout
+//        TO-DO (3) Use findViewById to get a reference to the TextView from the layout
+        eToysTextView = findViewById(R.id.tv_toy_names);
 
-//        TODO (4) Use the static ToyBox.getToyNames method and store the names in a String array
+//        TO-DO (4) Use the static ToyBox.getToyNames method and store the names in a String array
+        String [] toyNames = ToyBox.getToyNames();
 
-//        TODO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
+//        TO-DO (5) Loop through each toy and append the name to the TextView (add \n for spacing)
+        for (String toyName:toyNames) {
+            eToysTextView.append(toyName + "\n");
+        }
     }
 }
